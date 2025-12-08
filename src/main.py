0f79dfb4d81bcc66f8200aadb808537a1da7330e
WilliamBianchi61 <william.bianchi61@thg.com>
2025-12-08 16:26:26 +0000

    updated code with bin type selector

---
+++ src/main.py
@@ -0,0 +1,13 @@
+# import dump of packages
+import numpy as np
+import pandas as pd
+import math
+import os
+
+#imports from
+
+from product_fit import bin_type_selector
+
+# combine all functions here to start program
+
+bin_type_selector()
--- src/product_fit.py
+++ src/product_fit.py
@@ -5,22 +5,36 @@ import os
 
 csv = "<file_path>"
 
-autostore_bin_type = input("Input the Bin type: \n") # can make this into an automation type selector so GEEK and t-Sort can be included
+def bin_type_selector():
 
-autoStore_bin_size = [3]
+    autostore_bin_type = input("Input the Bin type: \n") # can make this into an automation type selector so GEEK and t-Sort can be included
 
-match autostore_bin_type:
-    case "220":
-        autoStore_bin_size = 220,400,600
-    case "330":
-        autoStore_bin_size = 330,400,600
-    case "425":
-        autoStore_bin_size = 425,400,600
-    case _:
-        print("no bins type selected")
+    autoStore_bin_size = [3]
+    
+    unit_type = input("What units: ")
+
+    match autostore_bin_type:
+        case "220":
+            autoStore_bin_size = 220,400,600
+        case "330":
+         autoStore_bin_size = 330,400,600
+        case "425":
+            autoStore_bin_size = 425,400,600
+        case _:
+            print("no bins type selected")
+
+    match unit_type:
+        case "mm":
+            print("is mm")
+        case "cm":
+            t = autoStore_bin_size
+            t2 = tuple(t/10 for ti in t)
+            autoStore_bin_size = t2
+            print("is cm")
+
+        case "m":
+            autoStore_bin_size /1000 , print('is m')
+
     
 
-
-
-
-print(autoStore_bin_size)
+    print(autoStore_bin_size)
